068e6846abb0b0ced96363a5ff31b9989592da25
Spencer <snsallay@gmail.com>
2015-01-25 21:24:10 -0500

    essentially the stdlib of ULCL; name comes from the name of Haskell's standard library.

---
+++ prelude.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+int add(int a0, int a1) {
+a1+a0;
+return ; }
+int sub(int a0, int a1) {
+a1-a0;
+return ; }
+int ti(int a0, int a1) {
+a1*a0;
+return ; }
+int div(int a0, int a1) {
+a1/a0;
+return ; }
+
---
+++ prelude.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+int add(int a0, int a1);
+int sub(int a0, int a1);
+int ti(int a0, int a1);
+int div(int a0, int a1);
