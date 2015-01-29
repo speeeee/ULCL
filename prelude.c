af973e85862eb2cf846be76b50a57998c0c74244
Spencer <snsallay@gmail.com>
2015-01-29 18:26:36 -0500

    add more to prelude

--- prelude.c
+++ prelude.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
 int add(int a0, int a1) {
-a1+a0;
+return a1+a0;
 return ; }
 int sub(int a0, int a1) {
-a1-a0;
+return a1-a0;
 return ; }
 int ti(int a0, int a1) {
-a1*a0;
+return a1*a0;
 return ; }
 int div(int a0, int a1) {
-a1/a0;
+return a1/a0;
+return ; }
+void* nth(int a0, int* a1) {
+return a1[a0];
 return ; }
 
--- prelude.h
+++ prelude.h
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
 int add(int a0, int a1);
 int sub(int a0, int a1);
 int ti(int a0, int a1);
 int div(int a0, int a1);
+void* nth(int a0, int* a1);
