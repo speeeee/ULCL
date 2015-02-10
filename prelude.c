4eecf081281fc74034fbbd72a0fea7410a4f5176
Spencer <snsallay@gmail.com>
2015-02-09 21:24:49 -0500

    fixed for newer version of compiler.

--- prelude.c
+++ prelude.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 int add(int a0, int a1) {
 return a1+a0;
-return ; }
+; }
 int sub(int a0, int a1) {
 return a1-a0;
-return ; }
+; }
 int ti(int a0, int a1) {
 return a1*a0;
-return ; }
+; }
 int div(int a0, int a1) {
 return a1/a0;
-return ; }
+; }
 bool equ(int a0, int a1) {
 return a1==a0;
-return ; }
+; }
 bool les(int a0, int a1) {
 return a1<a0;
-return ; }
+; }
 bool grt(int a0, int a1) {
 return a1>a0;
-return ; }
+; }
 void* nth(int a0, int* a1) {
 return a1[a0];
-return ; }
+; }
 
