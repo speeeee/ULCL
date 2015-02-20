94a90992afbc2ae551d04309994e7c6b69e9fa66
Spencer <snsallay@gmail.com>
2015-02-19 22:36:05 -0500

    successful attempt at making a simple triangle in OpenGL.

---
+++ gltest.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include "prelude.h"
+void paint() {
+  glClearColor(0.3,0.3,0.3,0.0);
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  glShadeModel(GL_SMOOTH);
+  glLoadIdentity();
+  glTranslatef(-15.0,-15.0,0.0);
+  glBegin(GL_TRIANGLES);
+  glColor3f(1.0,0.0,0.0);
+  glVertex2f(0.0,0.0);
+  glColor3f(0.0,1.0,0.0);
+  glVertex2f(30.0,0.0);
+  glColor3f(0.0,0.0,1.0);
+  glVertex2f(0.0,30.0);
+  glEnd();
+  glFlush();
+; }
+void reshape(int a0, int a1) {
+int height = a1;
+int width = a0;
+  glViewport(0,0,width,height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(-30.0,30.0,-30.0,30.0,-30.0,30.0);
+  glMatrixMode(GL_MODELVIEW);
+; }
+int main(int argc, char **argv) {  
+glutInit(adr(argc),argv);
+glutInitWindowSize(640,480);
+glutCreateWindow("Triangle");
+glutDisplayFunc(paint);
+glutReshapeFunc(reshape);
+glutMainLoop();
+}
+
---
+++ gltest.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include "prelude.h"
+void paint();
+void reshape(int a0, int a1);
