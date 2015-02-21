71ba6498e0751447f0af224e49e16eea700c45ac
Spencer <snsallay@gmail.com>
2015-02-20 20:45:48 -0500

    forgot that C doesn't normally have the 'bool' type.  oops.

--- prelude.c
+++ prelude.c
@@ -12,13 +12,13 @@ return a1*a0;
 int div(int a0, int a1) {
 return a1/a0;
 ; }
-bool equ(int a0, int a1) {
+int equ(int a0, int a1) {
 return a1==a0;
 ; }
-bool les(int a0, int a1) {
+int les(int a0, int a1) {
 return a1<a0;
 ; }
-bool grt(int a0, int a1) {
+int grt(int a0, int a1) {
 return a1>a0;
 ; }
 void* nth(int a0, int* a1) {
